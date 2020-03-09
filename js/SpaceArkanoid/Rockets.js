eb22834db17d46370470f6e13f8dbb3f1aa82ca7
unknown <alex>
2020-03-09 17:26:01 +0300

    1

--- js/SpaceArkanoid/Rockets.js
+++ js/SpaceArkanoid/Rockets.js
@@ -3,10 +3,10 @@
     Point,
     CircleCollision,
     alwaysMove
-} from "js/2DGameEngine.js";
+} from "./js/2DGameEngine.js";
 
-import { resources } from "js/SpaceArkanoid/GameResources.js";
-import { ExplosionBig_001 } from "js/SpaceArkanoid/Explosions.js";
+import { resources } from "./js/SpaceArkanoid/GameResources.js";
+import { ExplosionBig_001 } from "./js/SpaceArkanoid/Explosions.js";
 
 export class Rocket extends GameObject {
     constructor(img, master, spavnPoint, radius, damage) {
